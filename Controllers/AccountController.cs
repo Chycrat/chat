f3f9331710638fac1f71ca5ad80dc92416cde1cc
CHRETIEN Romain <romain.chretien@vinci-energies.com>
2020-02-14 15:49:34 +0100

    mise à jour du front $

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -422,7 +422,6 @@ namespace ChatCESI.Controllers
 
             base.Dispose(disposing);
         }
-
         #region Applications auxiliaires
         // Utilisé(e) pour la protection XSRF lors de l'ajout de connexions externes
         private const string XsrfKey = "XsrfId";
